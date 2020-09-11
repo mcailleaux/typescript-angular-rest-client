06f88ed52e4ddd36d44530598464111a603c637b
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2020-09-11 08:45:47 +0200

    create build and package script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Delete dist
+rm -fR dist
+
+# node dependencies
+npm ci
+
+# Check lint
+npm run lint
+
+# Build project
+npm run build
+
+# Prepare Npm package
+cp -r ./node_modules/typescript-rest-client/src/* ./dist/src/
+cp ./package.json ./dist/
+cp ./README.md ./dist/
+cp ./LICENSE ./dist/
